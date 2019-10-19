b4a0ab727a653bf17aba63c50e76f76e0f558406
Richard Salvaty <richardsalvaty@hotmail.com>
2019-10-19 12:08:29 -0700

    Update RecursiveFibonacci.cs
    method name refactor

--- Fibonacci_Recursion_Iterative_DynamicProgramming/RecursiveFibonacci.cs
+++ Fibonacci_Recursion_Iterative_DynamicProgramming/RecursiveFibonacci.cs
@@ -3,13 +3,13 @@
     public static class RecursiveFibonacci
     {
         //Get the "nth" Fibonacci number recursively
-        public static int fib(int n, int num, int num1)
+        public static int Fib(int n, int num, int num1)
         {
             if (n == 0)
                 return num;
             if (n == 1)
                 return num1;
-            return fib(n - 1, num1, num + num1);
+            return Fib(n - 1, num1, num + num1);
         }
     }
 }
