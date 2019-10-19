f561365352a3eda1b3e93cc7ed88c3000713ef35
Richard Salvaty <richardsalvaty@hotmail.com>
2019-10-19 12:09:02 -0700

    Update Program.cs
    removed unused args, applied method name refactoring

--- Fibonacci_Recursion_Iterative_DynamicProgramming/Program.cs
+++ Fibonacci_Recursion_Iterative_DynamicProgramming/Program.cs
@@ -4,13 +4,13 @@ namespace Fibonacci_Recursion_Iterative_DynamicProgramming
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             //Get the recursive values
-            var firstRecFib = RecursiveFibonacci.fib(1, 0, 1);
-            var secondRecFib = RecursiveFibonacci.fib(6, 0, 1);
-            var thirdRecFib = RecursiveFibonacci.fib(10, 0, 1);
-            var fourthRecFib = RecursiveFibonacci.fib(20, 0, 1);
+            var firstRecFib = RecursiveFibonacci.Fib(1, 0, 1);
+            var secondRecFib = RecursiveFibonacci.Fib(6, 0, 1);
+            var thirdRecFib = RecursiveFibonacci.Fib(10, 0, 1);
+            var fourthRecFib = RecursiveFibonacci.Fib(20, 0, 1);
 
             //Get the iterative values
             var iterativeFib1 = IterativeFibonacci.Fib(1);
@@ -19,10 +19,10 @@ namespace Fibonacci_Recursion_Iterative_DynamicProgramming
             var iterativeFib4 = IterativeFibonacci.Fib(20);
 
             //Get the dynamic recursive values
-            var dynamicFib1 = DynamicRecursiveFib.fib(1);
-            var dynamicFib2 = DynamicRecursiveFib.fib(6);
-            var dynamicFib3 = DynamicRecursiveFib.fib(10);
-            var dynamicFib4 = DynamicRecursiveFib.fib(20);
+            var dynamicFib1 = DynamicRecursiveFib.Fib(1);
+            var dynamicFib2 = DynamicRecursiveFib.Fib(6);
+            var dynamicFib3 = DynamicRecursiveFib.Fib(10);
+            var dynamicFib4 = DynamicRecursiveFib.Fib(20);
 
             Console.WriteLine("***************\nFibonacci Recursion: \nn = 1:\t{0}\nn = 6:\t{1}\nn = 10:\t{2}\nn = 20:\t{3}\n***************", firstRecFib, secondRecFib, thirdRecFib, fourthRecFib);
             Console.WriteLine("Fibonacci Iteration: \nn = 1:\t{0}\nn = 6:\t{1}\nn = 10:\t{2}\nn = 20:\t{3}\n***************", iterativeFib1, iterativeFib2, iterativeFib3, iterativeFib4);
