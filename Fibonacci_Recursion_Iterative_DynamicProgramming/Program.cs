76e1317a3a5953f7bb6bd6f76cba5e0463c12613
Richard Salvaty <richardsalvaty@hotmail.com>
2019-10-19 11:42:53 -0700

    Update Program.cs

--- Fibonacci_Recursion_Iterative_DynamicProgramming/Program.cs
+++ Fibonacci_Recursion_Iterative_DynamicProgramming/Program.cs
@@ -6,10 +6,18 @@ using System.Threading.Tasks;
 
 namespace Fibonacci_Recursion_Iterative_DynamicProgramming
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            int firstRecFib, secondRecFib, thirdRecFib, fourthRecFib;
+
+            firstRecFib = RecursiveFibonacci.fib(1, 0, 1);
+            secondRecFib = RecursiveFibonacci.fib(6, 0, 1);
+            thirdRecFib = RecursiveFibonacci.fib(10, 0, 1);
+            fourthRecFib = RecursiveFibonacci.fib(20, 0, 1);
+
+            Console.WriteLine("********************\nFibonacci Recirsion: \nn = 1:\t{0}\nn = 6:\t{1}\nn = 10:\t{2}\nn = 20:\t{3}\n********************", firstRecFib, secondRecFib, thirdRecFib, fourthRecFib);
         }
     }
 }
