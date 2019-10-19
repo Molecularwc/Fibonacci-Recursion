ba470162442cc5304b1d996d137229ee973da9ed
Richard Salvaty <richardsalvaty@hotmail.com>
2019-10-19 11:27:59 -0700

    Update RecursiveFibonacci.cs
    added static fib method

--- Fibonacci_Recursion_Iterative_DynamicProgramming/RecursiveFibonacci.cs
+++ Fibonacci_Recursion_Iterative_DynamicProgramming/RecursiveFibonacci.cs
@@ -8,5 +8,13 @@ namespace Fibonacci_Recursion_Iterative_DynamicProgramming
 {
     public static class RecursiveFibonacci
     {
+        public static int fib(int n, int num, int num1)
+        {
+            if (n == 0)
+                return num;
+            if (n == 1)
+                return num1;
+            return fib(n - 1, num1, num + num1);
+        }
     }
 }
