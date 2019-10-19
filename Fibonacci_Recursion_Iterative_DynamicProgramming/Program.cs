fc46772d274a95d9b52d71f941cb432b9df43999
Richard Salvaty <richardsalvaty@hotmail.com>
2019-10-19 11:55:12 -0700

    Update Program.cs
    joined declarations and assignments, added outputs for Iteration and Dynamic

--- Fibonacci_Recursion_Iterative_DynamicProgramming/Program.cs
+++ Fibonacci_Recursion_Iterative_DynamicProgramming/Program.cs
@@ -10,14 +10,27 @@ namespace Fibonacci_Recursion_Iterative_DynamicProgramming
     {
         private static void Main(string[] args)
         {
-            int firstRecFib, secondRecFib, thirdRecFib, fourthRecFib;
+            var firstRecFib = RecursiveFibonacci.fib(1, 0, 1);
+            var secondRecFib = RecursiveFibonacci.fib(6, 0, 1);
+            var thirdRecFib = RecursiveFibonacci.fib(10, 0, 1);
+            var fourthRecFib = RecursiveFibonacci.fib(20, 0, 1);
 
-            firstRecFib = RecursiveFibonacci.fib(1, 0, 1);
-            secondRecFib = RecursiveFibonacci.fib(6, 0, 1);
-            thirdRecFib = RecursiveFibonacci.fib(10, 0, 1);
-            fourthRecFib = RecursiveFibonacci.fib(20, 0, 1);
+            var iterativeFib1 = IterativeFibonacci.Fib(1);
+            var iterativeFib2 = IterativeFibonacci.Fib(6);
+            var iterativeFib3 = IterativeFibonacci.Fib(10);
+            var iterativeFib4 = IterativeFibonacci.Fib(20);
+
+            var dynamicFib1 = DynamicRecursiveFib.fib(1);
+            var dynamicFib2 = DynamicRecursiveFib.fib(6);
+            var dynamicFib3 = DynamicRecursiveFib.fib(10);
+            var dynamicFib4 = DynamicRecursiveFib.fib(20);
 
             Console.WriteLine("********************\n\nFibonacci Recursion: \nn = 1:\t{0}\nn = 6:\t{1}\nn = 10:\t{2}\nn = 20:\t{3}\n\n********************\n\n", firstRecFib, secondRecFib, thirdRecFib, fourthRecFib);
+
+            Console.WriteLine("********************\n\nFibonacci Iteration: \nn = 1:\t{0}\nn = 6:\t{1}\nn = 10:\t{2}\nn = 20:\t{3}\n\n********************\n\n", iterativeFib1, iterativeFib2, iterativeFib3, iterativeFib4);
+
+            Console.WriteLine("********************\n\nFibonacci Dynamic Recursion: \nn = 1:\t{0}\nn = 6:\t{1}\nn = 10:\t{2}\nn = 20:\t{3}\n\n********************\n\n", dynamicFib1, dynamicFib2, dynamicFib3, dynamicFib4);
+
             Console.ReadLine();
         }
     }
